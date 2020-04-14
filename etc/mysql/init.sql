01a784f25e4effd28a3c0b03446c51aa925a7c58
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-04-14 11:56:07 -0500

    update sql init file

--- etc/mysql/init.sql
+++ etc/mysql/init.sql
@@ -1,4 +1,3 @@
-CREATE USER gophish@'mysql' IDENTIFIED BY 'password';
-CREATE DATABASE gophish CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE gophish CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON gophish.* TO gophish@'mysql' WITH GRANT OPTION;
 SET GLOBAL CONFIG
